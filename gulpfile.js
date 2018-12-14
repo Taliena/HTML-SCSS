d70bea5b20f7633306c4e41c5ff507bc5528e5bd
Taliena <39599862+Taliena@users.noreply.github.com>
2018-12-14 14:05:14 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+﻿
+
+
+var gulp = require("gulp");
+var scss = require("gulp-sass");
+var browserSync = require('browser-sync');
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'project'
+        },
+        notify:false
+    });
+});
+gulp.task("compile", function () {
+    gulp.src("project/*.scss").pipe(scss())
+        .pipe(gulp.dest("project")).pipe(browserSync.reload({ stream: true }));
+
+    });
+gulp.task("watch", ['compile', 'browser-sync'], function () {
+    gulp.watch("project/*.scss", ["compile"]);
+    gulp.watch('project/*.html', browserSync.reload);
+});
